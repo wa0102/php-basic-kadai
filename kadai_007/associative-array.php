7e682a15d0775f9ca21398192288f2da19e7d123
wa0102 <minon.maro@gmail.com>
2026-01-07 16:23:40 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列を作ってキーと値を出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $associative_array = ["name" => "onion", "price" => 200, "weight" => 160];
+    print_r($associative_array);
+    ?>
+  </p>
+</body>
+
+</html>
