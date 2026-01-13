1d5ce5c5c168bd88b54fddbe4b667d6906cb1f94
wa0102 <minon.maro@gmail.com>
2026-01-13 17:35:07 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>クラスを2つ作ってそれぞれのインスタンスを出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    class Food{
+      private $name;
+      private $price;
+
+      public function __construct(string $name, int $price){
+        $this->name = $name;
+        $this->price = $price;
+      }
+      public function show_price(){
+        echo $this->price . "<br>";
+      }
+    }
+
+    $Price = new Food("カレー", 500);
+
+    $Price->show_price();
+    print_r($Price);
+    ?>
+  </p>
+  <p>
+    <?php
+    class Animal{
+      private $name;
+      private $height;
+      private $weight;
+      
+      public function __construct(string $name, int $height, int $weight){
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+      }
+      public function show_height(){
+        echo $this->height . "<br>";
+      }
+    }
+
+    $Height = new Animal("犬", 1, 1);
+
+    $Height->show_height();
+    print_r($Height);
+    ?>
+  </p>
+</body>
+
+</html>
