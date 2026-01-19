8735be06de87c6b4bd7d3ab242db7979417149ba
wa0102 <minon.maro@gmail.com>
2026-01-19 09:25:23 +0900

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,31 @@
+<?php
+// セッションを開始
+session_start();
+
+// セッションに保存された「社員名」を取得
+$employee_name = isset($_SESSION["employee_name"]) ? $_SESSION["employee_name"] : "名無し";
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>課題 社員情報入力フォームを作成しよう</title>
+</head>
+
+<body>
+  <h2>登録が完了しました。</h2>
+  <p>社員情報がデータベースに保存されました。</p>
+  <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+
+  <?php
+  // セッション変数を初期化
+  $_SESSION = array();
+
+  // セッションを終了
+  session_destroy();
+  ?>
+</body>
+
+</html>
